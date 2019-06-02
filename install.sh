e7192e4b006cff90feb3b403299532cf042a4093
Markus Haas <markushaas2003@gmail.com>
2019-06-02 16:44:47 +0200

    Fixed the Linux Installer

--- install.sh
+++ install.sh
@@ -1,4 +1,1 @@
-#!/usr/bin/env bash
-pip install -r Sublist3r/requirements.txt && 
-python Sublist3r/setup.py install && 
-python SubPortScanner.py
+pip install -r Sublist3r/requirements.txt && python Sublist3r/setup.py install && python SubPortScanner.py
