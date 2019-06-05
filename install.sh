0a97a4621e5f599fe907fcfab2fdef962ae24b25
Markus Haas <markushaas2003@gmail.com>
2019-06-05 21:51:05 +0200

    Added sudo.

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-pip install -r Sublist3r/requirements.txt && python Sublist3r/setup.py install && python SubPortScanner.py
+sudo pip install -r Sublist3r/requirements.txt && sudo python Sublist3r/setup.py install && python SubPortScanner.py
